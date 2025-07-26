f754e45c15e8e0ae9f9c956d720c277bb7406044
Faith Nwankwo <nkediya1948@gmail.com>
2025-07-25 22:09:44 -0600

    Fix: Correct function names and datetime import for explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,11 +6,11 @@ def display_current_datetime():
 
 def calculate_future_date():
     try:
-        days_to_add = int(input("Enter the number of days to add to the current date: "))
-        future_date = datetime.datetime.now() + datetime.timedelta(days=days_to_add)
+        days = int(input("Enter the number of days to add to the current date: "))
+        future_date = datetime.datetime.now() + datetime.timedelta(days=days)
         print("Future date:", future_date.strftime("%Y-%m-%d"))
     except ValueError:
-        print("Please enter a valid number.")
+        print("Please enter a valid integer.")
 
 if __name__ == "__main__":
     display_current_datetime()
