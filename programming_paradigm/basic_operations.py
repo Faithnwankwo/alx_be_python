2a31a54ea68a7c5678eafdd6803f42b0b8d68d27
Faith Nwankwo <nkediya1948@gmail.com>
2025-08-02 15:12:17 -0600

    Fix submodule issue and add BankAccount files

---
+++ programming_paradigm/basic_operations.py
@@ -0,0 +1,8 @@
+# Define two variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations and print results
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
